d25a250ff0a2f0b3694b12dffd44f3eb30775c4d
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-13 17:05:21 +0530

    Create band_name.py

---
+++ Projects/Band name generator/code.py
@@ -0,0 +1,4 @@
+print('Welcome to Band Name Generator !!!')
+City = input('May i know your Birth place ?\n')
+Pet = input('May i know your Pet name ?\n')
+print(City + ' ' + Pet + " is your new Band Name")
