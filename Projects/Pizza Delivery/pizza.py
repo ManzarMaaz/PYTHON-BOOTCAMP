eb2fb7d317fe3405835e4e83d935f2b9fdbb41d7
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-14 16:15:09 +0530

    Create pizza.py

---
+++ Projects/Pizza Delivery/pizza.py
@@ -0,0 +1,26 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill = 0
+
+if size in ('S', "s"):
+    bill += 15
+elif size in ('M', 'm'):
+    bill += 20
+elif size in ('L','l'):
+    bill += 25
+
+if pepperoni in ('Y','y'):
+    print('Added Pepperoni !!')
+    if size in ('S',"s"):
+        bill += 2
+    elif size in ('M','m','l','L'):
+        bill += 3
+
+if extra_cheese in ('Y','y'):
+    print('Added Extra Cheese !!')
+    bill += 1
+
+print(f'Your final bill is: ${bill}.')
