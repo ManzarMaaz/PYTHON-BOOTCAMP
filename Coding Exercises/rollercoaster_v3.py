f94dd50d0b9ed3845b0bb6796f993a4921b04b4a
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-14 15:54:52 +0530

    Update rollercoaster_v3.py

--- Coding Exercises/rollercoaster_v3.py
+++ Coding Exercises/rollercoaster_v3.py
@@ -1,25 +1,27 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
-    picture = input('If you want your Ride Picture cost Extra $3 ? (Y/N): ')
 
     if age <= 12:
-        if picture in ("Y",'y'):
-            print("Please pay $8 :) ")
-        else:
-            print("Please pay $5.")
+        bill = 5
+        print("Child Tickets are $5.")
+
     elif age <= 18:
-        if picture in ("Y", 'y'):
-            print("Please pay $10 :) ")
-        else:
-            print("Please pay $7.")
+        bill = 7
+        print("Youth Tickets are $7.")
+
     else:
-        if picture in ("Y", 'y'):
-            print("Please pay $15 :) ")
-        else:
-            print("Please pay $12.")
+        bill = 12
+        print("Adult Tickets are $12.")
+
+    picture = input('If you want your Ride Picture, It costs Extra $3 ? (Y/N): ')
+    if picture in ('Y','y'):
+        bill += 3
+    print(f'Your Final Bill is ${bill}')
+
 else:
     print("Sorry you have to grow taller before you can ride.")
