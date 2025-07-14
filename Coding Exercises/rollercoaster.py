6b8d73f02ecd1a14c3db2cad649644635db377ff
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-14 15:07:59 +0530

    Create rollercoaster.py

---
+++ Coding Exercises/rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to the Rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print('Hurry, You can Ride the Rollercoaster !!!')
+else:
+    print('Oh, You are not Eligible ')
