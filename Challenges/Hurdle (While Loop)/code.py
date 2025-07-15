437b1c5f68818ad0ec3fe685c36f57797cae2654
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-15 20:06:14 +0530

    Create code.py

---
+++ Challenges/Hurdle (While Loop)/code.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    for x in range(6):
+        jump()
+    else:
+        break
