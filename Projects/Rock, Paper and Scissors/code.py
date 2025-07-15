a692f197c0cdb095273200d865e5e30d12dcb70c
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-15 12:41:22 +0530

    Update code.py

--- Projects/Rock, Paper and Scissors/code.py
+++ Projects/Rock, Paper and Scissors/code.py
@@ -30,14 +30,15 @@ scissors = '''
 ---.__(___)
 '''
 images = [rock,paper,scissors]
+images_names = ['Rock','Paper','Scissors']
 my_choice = int(input('Choose 0 for Rock, 1 for Paper or 2 for Scissors ?\n'))
 if my_choice not in (0,1,2):
     print('Invalid Input !!!')
 
 else:
-    print('You Chose:\n',images[my_choice])
+    print(f'You Chose : {images_names[my_choice]}\n',images[my_choice])
     computer = randint(0, 2)
-    print('Your Opponent chose:\n', images[computer])
+    print(f'Your Opponent chose : {images_names[computer]}\n', images[computer])
 
     if (my_choice == 1 and computer == 2) or (my_choice == 3 and computer == 2) or (my_choice == 2 and computer == 1):
         print(f'You Wins !!!')
