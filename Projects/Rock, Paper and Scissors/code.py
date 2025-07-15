4f9a59e895a5ad557bf70ce31492eaeae8ba7642
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-07-15 12:35:22 +0530

    Create code.py

---
+++ Projects/Rock, Paper and Scissors/code.py
@@ -0,0 +1,48 @@
+import random
+from random import randint
+
+print("********** WELCOME TO ROCK, PAPER AND SCISSORS GAME **********")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+images = [rock,paper,scissors]
+my_choice = int(input('Choose 0 for Rock, 1 for Paper or 2 for Scissors ?\n'))
+if my_choice not in (0,1,2):
+    print('Invalid Input !!!')
+
+else:
+    print('You Chose:\n',images[my_choice])
+    computer = randint(0, 2)
+    print('Your Opponent chose:\n', images[computer])
+
+    if (my_choice == 1 and computer == 2) or (my_choice == 3 and computer == 2) or (my_choice == 2 and computer == 1):
+        print(f'You Wins !!!')
+    elif my_choice == computer:
+        print('Its a Draw !!!')
+    else:
+        print(f'You Lost !!!')
+
