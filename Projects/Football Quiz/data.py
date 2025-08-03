4ddb3b50924b727babe5ae90ae639b1830382dfe
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-08-03 12:34:07 +0530

    Add files via upload

---
+++ Projects/Football Quiz/data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "Lionel Messi has won the Ballon d'Or more times than Cristiano Ronaldo.", "answer": "True"},
+    {"text": "The FIFA World Cup is held every 3 years.", "answer": "False"},
+    {"text": "Brazil has won the most FIFA World Cup titles.", "answer": "True"},
+    {"text": "A standard football match lasts 100 minutes.", "answer": "False"},
+    {"text": "The 2022 FIFA World Cup was held in Qatar.", "answer": "True"},
+    {"text": "The Premier League is played in Spain.", "answer": "False"},
+    {"text": "The position of a goalkeeper is the only one allowed to use hands during play.", "answer": "True"},
+    {"text": "Diego Maradona scored a goal known as the 'Hand of God'.", "answer": "True"},
+    {"text": "The offside rule does not apply during a goal kick.", "answer": "True"},
+    {"text": "Manchester United's home ground is Anfield.", "answer": "False"},
+    {"text": "Zinedine Zidane headbutted Marco Materazzi in the 2006 World Cup Final.", "answer": "True"},
+    {"text": "A red card means the player must leave the field and can be replaced by a substitute.", "answer": "False"}
+]
