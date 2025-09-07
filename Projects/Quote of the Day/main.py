c28df396e8dfc56199b41ad53d6441325d5f8361
Mohammad Manzar Maaz <167881132+ManzarMaaz@users.noreply.github.com>
2025-09-07 12:27:16 +0530

    Create main.py

---
+++ Projects/Quote of the Day/main.py
@@ -0,0 +1,20 @@
+import random
+import smtplib
+import datetime as dt
+
+my_mail='testingmmm55@gmail.com'
+password="kirfilkpygihdpwm"
+
+now = dt.datetime.now()
+today = now.weekday()
+
+with open('DAY 32/Birthday Wisher (Day 32) start/quotes.txt', 'r') as file:
+    quotes_list = file.readlines()
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_mail, password=password)
+        if today in [0,1,2,3,4,5,6]:
+            connection.sendmail(from_addr=my_mail,
+                            to_addrs='manzar01012003@gmail.com',
+                            msg=f'subject:Quote of the Day\n\n{random.choice(quotes_list)}')
